6cbaaf646584a3912f065016c63800c326d12487
push9828 <pushkar.thakur28@gmail.com>
2024-09-23 15:27:50 +0530

    initial setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,47 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.(".svg")
+    );
+
+    config.module.rules.push(
+      // Convert SVG to React components
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      // Convert all other *.svg imports to React components
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+        use: {
+          loader: "@svgr/webpack",
+          options: {
+            svgoConfig: {
+              plugins: [
+                {
+                  name: "preset-default",
+                  params: {
+                    overrides: {
+                      removeViewBox: false,
+                    },
+                  },
+                },
+              ],
+            },
+          },
+        },
+      }
+    );
+
+    // Exclude *.svg files from being processed by Next.js
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
+  },
+};
 
 export default nextConfig;
