ef5ea4c73b86dced67587474a891299cd1b613fc
push9828 <pushkar.thakur28@gmail.com>
2024-10-16 14:49:00 +0530

    add logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,12 @@
 import { CiMenuFries } from "react-icons/ci";
+import Logo from "@/assets/logo.png";
+import Image from "next/image";
 
 export const NavBar = () => {
   return (
-    <header className=" py-5  sticky top-0 backdrop-blur-sm z-20 w-full">
+    <header className="py-5  sticky top-0 backdrop-blur-sm z-20 w-full">
       <div className="container flex items-center justify-between">
-        <div>Logo</div>
+        <Image src={Logo} alt="logo" width={50} height={50} />
         <CiMenuFries className="md:hidden" />
         <nav className="hidden md:flex gap-6 text-black/80 items-center">
           <a href="#home">Home</a>
